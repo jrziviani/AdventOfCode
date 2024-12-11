fd3f4cca881e99fd7c6c87ace9a0d36918f8b809
Jose R. Ziviani <jose@ziviani.net>
2024-12-11 19:23:00 -0300

    Add correct day9 cpp

--- day_9/day9.cpp
+++ day_9/day9.cpp
@@ -5,131 +5,16 @@
 
 using namespace std;
 
-struct aocfile {
-    int id;
-    int size;
-    string repr;
 
-    aocfile(int id, int size, string repr) : id(id), size(size), repr(repr) {}
+using zchar = vector<int>;
+using zstring = vector<vector<int>>;
 
-    string getrepr() const {
-        return repr;
-    }
-
-    string getid() const {
-        if (id == -1) {
-            return ".";
-        }
-        else {
-            return to_string(id);
-        }
-    }
-};
-
-using files = vector<aocfile>;
-
-files expand(const string &input) {
-    files output;
-
+long long int checksum(const zstring &input) {
+    long long int sum = 0;
     int k = 0;
     for (int i = 0; i < input.size(); ++i) {
-        int ntimes = input[i] - '0';
-        if (i & 1 == 1) {
-            for (int j = 0; j < ntimes; ++j) 
-                output.push_back(aocfile(-1, ntimes - j, "."));
-        }
-        else {
-            for (int j = 0; j < ntimes; ++j) 
-                output.push_back(aocfile(k, ntimes, "x"));
-            k++;
-        }
-    }
-
-    return output;
-}
-
-void print(const files &f) {
-    for (int i = 0; i < f.size(); ++i) {
-        cout << f[i].repr;
-    }
-    cout << endl;
-}
-
-void compact(files &input) {
-    auto is_free_space = [&input](int i) { return i >= 0 && i < input.size() && input[i].id == -1; };
-    auto has_free_capacity = [&input](int i, int j) { return i >= 0 &&
-                                                             i < input.size() &&
-                                                             j >= 0 &&
-                                                             j < input.size() &&
-                                                             input[i].id == -1 &&
-                                                             input[j].id != -1 &&
-                                                             input[j].size <= input[i].size; };
-    int k = input.size() - 1;
-    int i = 0;
-    for (int i = 0; i < input.size(); ++i) {
-        print(input);
-        if (!is_free_space(i)) {
-            continue;
-        }
-
-        int lastk = k;
-        for (int j = 0; j < k; j++) {
-            if (is_free_space(j) && has_free_capacity(j, k) && k >= j) {
-                auto id = input[k].id;
-                while (k >= 0 && input[k].id == id) {
-                    input[j].id = input[k].id;
-                    input[j].repr = input[k].repr;
-                    input[k].id = -1;
-                    input[k].repr = ".";
-                    k--;
-                    j++;
-                }
-                break;
-            }
-        }
-
-        if (k < 0) {
-            break;
-        }
-
-        if (lastk == k) {
-            auto id = input[k].id;
-            while (k >= 0 && input[k].id == id) {
-                k--;
-            }
-        }
-    }
-    // print(input);
-
-    long long int sum = 0;
-    for (i = 0; i < input.size(); ++i) {
-        if (input[i].id == -1) {
-            continue;
-        }
-        sum += input[i].id * i;
-    }
-
-     cout << sum << endl;
-}
-
-long long int checksum(files &input) {
-    long long int sum = 0;
-    int left = 0;
-    int right = input.size() - 1;
-    long long int k = 0;
-    while (left <= right) {
-        if (input[left].id == -1 && input[right].id == -1) {
-            right--;
-        }
-        else if (input[left].id == -1 && input[right].id != -1) {
-            sum += input[right].id * k;
-            left++;
-            k++;
-            right--;
-        }
-        else if (input[left].id != -1) {
-            sum += input[left].id * k;
-            left++;
+        for (int j = 0; j < input[i].size(); ++j) {
+            sum += input[i][j] * k;
             k++;
         }
     }
@@ -137,19 +22,155 @@ long long int checksum(files &input) {
     return sum;
 }
 
+zchar expand(int s, int n) {
+    zchar output(n);
+
+    for (int i = 0; i < n; ++i) {
+        output[i] = s;
+    }
+
+    return output;
+}
+
+zstring expand_fs(const string &input) {
+    zstring output;
+
+    for (size_t i = 0, j = 0; i < input.size(); ++i) {
+        int ntimes = input[i] - '0';
+
+        if (ntimes == 0) {
+            continue;
+        }
+        
+        if (i % 2 == 1) {
+            output.push_back(expand(-1, ntimes));
+        }
+        else {
+            output.push_back(expand(j, ntimes));
+            j++;
+        }
+    }
+
+    return output;
+}
+
+zstring compact(const zstring &input) {
+    int left = 0;
+    int i = 0;
+    int right = input.size() - 1;
+    int j = input[right].size() - 1;
+
+    zstring output;
+
+    zchar c;
+    while (left <= right) {
+        if (left == right && i > j) {
+            break;
+        }
+
+        if (j < 0) {
+            do {
+                right--;
+                j = input[right].size() - 1;
+            } while (j < 0);
+        }
+
+        if (input.size() < right && input[right].size() < j && input[right][j] == -1) {
+            j--;
+        }
+        else if (input.size() < left && input[left].size() < i && input[left][i] == -1) {
+            c.push_back(input[right][j]);
+            j--;
+            i++;
+        }
+        else {
+            c.push_back(input[left][i]);
+            i++;
+        }
+
+        if (i >= input[left].size()) {
+            i = 0;
+            left++;
+            output.push_back(c);
+            c.clear();
+        }
+    }
+
+    if (c.size() > 0) {
+        output.push_back(c);
+    }
+
+    return output;
+}
+
+
+zstring compact(zstring &input) {
+    int left = 0;
+    int i = 0;
+    int right = input.size() - 1;
+    int j = right >= 0 ? input[right].size() - 1 : -1; // Handle empty input.
+
+    zstring output;
+    zchar c;
+
+    while (left <= right) {
+        if (left == right && i > j) {
+            break;
+        }
+
+        // Skip empty rows from the right.
+        while (right >= 0 && j < 0) {
+            right--;
+            j = (right >= 0) ? input[right].size() - 1 : -1;
+        }
+
+        if (right < 0) { // If we've exhausted all elements on the right.
+            break;
+        }
+
+        // Copy or move elements from right to left.
+        if (i < input[left].size() && j >= 0 && input[right][j] != -1) {
+            input[left][i] = input[right][j];
+            c.push_back(input[right][j]);
+            j--;
+            i++;
+        } else if (i < input[left].size() && input[left][i] != -1) {
+            c.push_back(input[left][i]);
+            i++;
+        } else {
+            j--;
+        }
+
+        // Move to the next row from the left if the current row is done.
+        if (i >= input[left].size()) {
+            i = 0;
+            left++;
+            if (!c.empty()) {
+                output.push_back(c);
+                c.clear();
+            }
+        }
+    }
+
+    // Add any remaining characters to the output.
+    if (!c.empty()) {
+        output.push_back(c);
+    }
+
+    return output;
+}
+
+
 int main() {
     ifstream file("example.txt");
     string line;
     getline(file, line);
     file.close();
 
-    files expanded = expand(line);
-    //print(expanded);
+    auto expanded = expand_fs(line);
+    auto compacted = compact(expanded);
+    cout << checksum(compacted) << endl;
 
-    cout << checksum(expanded) << endl;
-
-    compact(expanded);
-    // print(expanded);
     
     return 0;
 }
